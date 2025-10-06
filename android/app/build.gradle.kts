14d6c67dec54177d5f6b904d18987a0760ecaf85
Petra Michael <petraxmichael@gmail.com>
2025-10-06 10:27:03 +0700

    trying to fix android release issue

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,12 +26,20 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
+    signingConfigs {
+        create("release") {
+            storeFile = file("qsafevault-release-key.jks") // Will be created by GitHub Actions
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release") // Use release key
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
