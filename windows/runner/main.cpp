3afac9c7e7e9561df1fce9ac4dc96381fbb0e9b4
Petra Michael <petraxmichael@gmail.com>
2025-11-02 13:16:04 +0700

    update workflow

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -8,6 +8,10 @@
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t* command_line, _In_ int show_command) {
 
+  // if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
+  //   CreateAndAttachConsole();
+  // }
+
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
   flutter::DartProject project(L"data");
